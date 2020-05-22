0af33eacd17b3ee19431f77290f7ce29951fe217
Alex Fischer <alex@quadrant.net>
2020-05-22 15:25:37 -0600

    Basics working

---
+++ back-or-up-button.js
@@ -0,0 +1,17 @@
+addEventListener('load', function() {
+    var placeholders = document.querySelectorAll('back-or-up-button');
+    if (NavTricks.previousPageIsInternal()) {
+        for (var i = placeholders.length - 1; i >= 0; i--) {
+            placeholders[i].outerHTML = '<button onclick="NavTricks.returnToPreviousPage()">&lt '+
+                NavTricks.previousPage.title || 'Back' +
+                '</button>';
+        }
+    }
+    else NavTricks.withParentPage(function(page) {
+        for (var i = placeholders.length - 1; i >= 0; i--) {
+            placeholders[i].outerHTML = '<a href="'+page.url+'">^ '+
+                page.title||'Parent Page' +
+                '<a>'
+        }
+    });
+});
