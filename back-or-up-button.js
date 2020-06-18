05b2b08422ca771dfbc4083f922c8cb9d0753d1e
Alex Fischer <alex@quadrant.net>
2020-06-18 11:03:32 -0600

    back-or-up-button fix

--- back-or-up-button.js
+++ back-or-up-button.js
@@ -3,14 +3,14 @@ addEventListener('load', function() {
     if (NavTricks.previousPageIsInternal()) {
         for (var i = 0; i < placeholders.length; i++) {
             placeholders[i].outerHTML = '<button onclick="NavTricks.returnToPreviousPage()" class="BackButton" aria-label="back">◀ '+
-                NavTricks.previousPage.title || 'Back' +
+                (NavTricks.previousPage.title || 'Back') +
                 '</button>';
         }
     }
     else NavTricks.withParentPage(function(page) {
         for (var i = 0; i < placeholders.length; i++) {
             placeholders[i].outerHTML = '<a href="'+page.path+'" class="ParentLink">▲ '+
-                page.title||'Parent Page' +
+                (page.title||'Parent Page') +
                 '<a>'
         }
     });
